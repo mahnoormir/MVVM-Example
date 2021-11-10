bb01e5dfd16f4361704cbab4398c45e5b2deeb85
mahnoormir <90832045+mahnoormir@users.noreply.github.com>
2021-11-11 03:14:01 +0500

    MVVM Example

--- app/src/main/java/com/example/mvvm/MainActivity.java
+++ app/src/main/java/com/example/mvvm/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.mvvm;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -7,19 +8,34 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.mvvm.adapters.PostAdapter;
 import com.example.mvvm.models.PostModel;
 import com.example.mvvm.retrofit.ApiInterface;
 import com.example.mvvm.retrofit.RetroClient;
 import com.example.mvvm.viewmodels.PostViewModel;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.navigation.NavigationView;
 
 import org.w3c.dom.Text;
@@ -30,12 +46,14 @@ import retrofit2.Call;
 
 public class MainActivity extends AppCompatActivity {
 
+
     private List<PostModel> postModelList;
     private PostAdapter adapter;
     private PostViewModel viewModel;
 
     DrawerLayout drawerLayout;
     NavigationView navigationView;
+    ActionBarDrawerToggle toggle;
     Toolbar myToolBar;
 
     @Override
@@ -43,19 +61,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         // implementing functionalities for side menu
         drawerLayout = findViewById(R.id.drawerlayout);
         navigationView = findViewById(R.id.navigationview);
         myToolBar = findViewById(R.id.myToolBar);
 
+
         //toolbar
         setSupportActionBar(myToolBar);
 
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout,myToolBar,R.string.navigation_open,R.string.navigation_close);
-
+        //drawer layout
+        toggle = new ActionBarDrawerToggle(this, drawerLayout,myToolBar,R.string.navigation_open,R.string.navigation_close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
 
+
+        //Nav Controller implementation
+        NavController navController = Navigation.findNavController(this,R.id.navHostFragment);
+        NavigationUI.setupWithNavController(navigationView,navController);
+
+
+        //Recycler view Implementation
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
 
         //set linear layout manager to recycler view
@@ -64,7 +91,6 @@ public class MainActivity extends AppCompatActivity {
 
         adapter = new PostAdapter(this,postModelList);
         recyclerView.setAdapter(adapter);
-
 
         viewModel = new ViewModelProvider(this).get(PostViewModel.class);
         viewModel.getPostData().observe(this, new Observer<List<PostModel>>() {
@@ -82,5 +108,15 @@ public class MainActivity extends AppCompatActivity {
         viewModel.makeApiCall();
 
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(toggle.onOptionsItemSelected(item))
+            return true;
+
+        return super.onOptionsItemSelected(item);
+    }
+
 
 }
---
+++ app/src/main/java/com/example/mvvm/fragments/LoginFragment.java
@@ -0,0 +1,146 @@
+package com.example.mvvm.fragments;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.mvvm.R;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class LoginFragment extends Fragment {
+
+    GoogleSignInClient mGoogleSignInClient;
+    Button sign_in_button_google;
+    LoginButton sign_in_button_fb;
+    private CallbackManager c;
+    private static int RC_SIGN_IN=100;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+
+        // Google Sign in Implementation
+        sign_in_button_google = view.findViewById(R.id.sign_in_button_google);
+        sign_in_button_google.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signIn();
+            }
+        });
+
+        //default sign in parameters
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(getActivity(), gso);
+
+        // Check for existing Google Sign In account, if the user is already signed in
+        // the GoogleSignInAccount will be non-null.
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(getActivity());
+
+
+        //Facebook sign in Implementation
+        sign_in_button_fb = view.findViewById(R.id.sign_in_button_fb);
+        c = CallbackManager.Factory.create();
+        sign_in_button_fb.setPermissions(Arrays.asList("email"));
+        sign_in_button_fb.setFragment(this);
+        sign_in_button_fb.registerCallback(c, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(@NonNull FacebookException e) {
+
+            }
+        });
+
+
+        return view;
+
+
+    }
+
+    //function for sign in
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            //getting profile information
+            // can be used for diplay once signed in successfully
+            GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(getActivity());
+            if (acct != null) {
+                String personName = acct.getDisplayName();
+                String personGivenName = acct.getGivenName();
+                String personFamilyName = acct.getFamilyName();
+                String personEmail = acct.getEmail();
+                String personId = acct.getId();
+                Uri personPhoto = acct.getPhotoUrl();
+            }
+
+            Toast.makeText(getContext(), "User Signed In ", Toast.LENGTH_SHORT).show();
+            
+        } catch (ApiException e) {
+            Log.d("message",e.toString());
+        }
+    }
+
+
+}
